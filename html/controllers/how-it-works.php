4d78edc3a0cd0bfcc77aac85a406c3158755beb5
webdev498 <gcmobile@mail.com>
2016-11-15 13:04:33 -0600

    update

--- html/controllers/how-it-works.php
+++ html/controllers/how-it-works.php
@@ -25,76 +25,130 @@ else{ $mainPage = ""; }
 				<div className="clearfix" id="page">
 					<div className="position_content" id="page_position_content">
 						<Header />
-						<div className="container-fluid middle-container">
-							<span id="backBtn" className="Text-1 clearfix colelem"><a href="/controllers/index.php"><span className="fa fa-chevron-left"></span> Home</a></span>
-						</div>
 						<div className="container-fluid">
-							<div className="container how_it_work">
-								<h2>How it works</h2>
-								<div className="col-xs-12 col-sm-3 column1">
-									<h3 className="text-popups"><span className="c_green">Select</span> your basic parameters</h3>
-									<h4 className="reducedWidth">Using simple sliders, make selections for four primary categories:</h4>
-									<h4 className="item-height">&nbsp;</h4>
-									<h4 className="item-height Text-2-ex-lead"><span className="cont_img"><img src="/images/price_range.png"/></span> Price range</h4>
-									<h4 className="item-height Text-2-ex-lead"><span className="cont_img"><img src="/images/number_of_bedroom.png"/></span> Number of bedrooms</h4>
-									<h4 className="item-height Text-2-ex-lead word-overflow"><span className="cont_img"><img src="/images/nightborhood.png"/></span> Neighborhoods</h4>
-									<h4 className="item-height Text-2-ex-lead"><span className="cont_img"><img src="/images/property_types.png"/></span> Property types</h4>
+							<div className="row-fluid">
+								<div className="clearfix colelem" id="pu336-4">
+									<div className="browser_width grpelem" id="u336-4-bw">
+										<div className="Subhead-2 clearfix" id="u336-4">
+											<h2>
+												<span>How it works</span>
+												<span id="backBtn" className="Text-1 clearfix colelem"><a href="/controllers/index.php"><span className="fa fa-chevron-left"></span> Home</a></span>
+											</h2>
+										</div>
+									</div>
 								</div>
-								<div className="col-xs-12 col-sm-3 column2">
-									<h3 className="text-popups"><span className="c_orange">Define</span> your buying formula</h3>
-									<h4>Using simple sliders, make selections for five primary motivators:</h4>
-									<h4 className="item-height">&nbsp;</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/location.png"/></span> Location grade</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/building.png"/></span> Building grade</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/ser.png"/></span> View grade</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/bed.png"/></span> Master bedroom size</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/sofa.png"/></span> Living room size</h4>
-									<p>...and desired amenities such as fireplace, laundry and outdoor space.</p>
-									<img className="amenityIcon" src="/images/icon_fireplace_2_30k_33px.png"/>
-									<img className="amenityIcon" src="/images/washer_3_37px.png"/>
-									<img className="amenityIcon" src="/images/icon_roofdeck_4_30k_33px.png"/>
-								</div>
-								<div className="col-xs-12 col-sm-3 column3">
-									<h3 className="text-popups"><span className="c_pink">Search</span> for apartments that match it</h3>
-									<span className="cont_img pinkArrow"><img src="/images/search_homepik.png"/></span>
-									<h4 className="pink_arrow"><span className="homepik">HomePik</span>&nbsp; will scan the entire database of available apartments in New York City for those that match your buying formula</h4>
-								</div>
-								<div className="col-xs-12 col-sm-3 column4">
-									<h3 className="text-popups"><span className="c_blue">View</span> the results of the search</h3>
-									<h4 className="item-height"><span className="homepik">HomePik</span> has located every apartment that meets your minimum criteria and compared them using its patented rating system:</h4>
-									<h4 className="item-height">&nbsp;</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/meets.png"/></span>  = meets your criteria</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/exceeds.png"/></span>  = exceeds your criteria</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/greatly.png"/></span>  = greatly exceeds your criteria</h4>
-										<img src="/images/divider_d.png"/>
-									<p className="text-center"><span className="c_blue_p">HomePik is the one and only search engine able to compare properties.</span> Your searches. Your way.</p>
-								</div>
-								<div className="col-xs-12 col-sm-3 border_right_none column5">
-									<h3 className="text-popups"><span className="c_purple">Manage</span> your searches and selections</h3>
-									<h4 className="item-height">Save searches and properties, email them, and connect with your agent</h4>
-									<h4 className="item-height">&nbsp;</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/save_to_folders.png"/></span> Save to folders</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/emails.png"/></span> Email</h4>
-									<h4 className="item-height"><span className="cont_img"><img src="/images/chose_your_agent.png"/></span> Choose an agent</h4>
-								</div>
+								<div className="col-md-5ths">
+                  <div className="panel">
+                    <div className="panel-body first-panel how-it-works-panel">
+                      <div className="clearfix grpelem" id="u332-24">
+												<h4 className="text-popups" id="u332-3"><span id="u332">Select</span><span id="u332-2"> your basic</span></h4>
+												<h4 className="text-popups">parameters</h4>
+												<p id="u332-6">&nbsp;</p>
+												<p className="Text-1">Using simple sliders,</p>
+												<p className="Text-1">make selections for four <br/>primary categories:<br/><br/></p>
+												<h6 className="Text-2-ex-lead" id="u332-16"><img className="selectImgs" src="/images/price_range.png"/> Price range</h6>
+												<h6 className="Text-2-ex-lead" id="u332-18"><img className="selectImgs" src="/images/number_of_bedroom.png"/> Number of bedrooms</h6>
+												<h6 className="Text-2-ex-lead" id="u332-20"><img className="selectImgs" src="/images/neighborhood.png"/> Neighborhoods</h6>
+												<h6 className="Text-2-ex-lead" id="u332-22"><img className="selectImgs" src="/images/property_types.png"/> Property types</h6>
+											</div>
+                    </div>
+                  </div>
+                </div>
+								<div className="col-md-5ths">
+                  <div className="panel">
+                    <div className="panel-body how-it-works-panel">
+                      <div className="clearfix grpelem" id="u331-24">
+												<h4 className="text-popups" id="u331-3"><span id="u331">Define</span><span id="u331-2"> your buying</span></h4>
+												<h4 className="text-popups">formula</h4>
+												<p>&nbsp;</p>
+												<p className="Text-1">Using simple sliders, make selections for five primary motivators:</p>
+												<p className="Text-1">&nbsp;</p>
+												<h6 className="Text-2-ex-lead" id="u331-11"><img className="defineImgs" src="/images/location.png"/> Location grade</h6>
+												<h6 className="Text-2-ex-lead" id="u331-13"><img className="defineImgs" src="/images/building.png"/> Building grade</h6>
+												<h6 className="Text-2-ex-lead" id="u331-15"><img className="defineImgs" src="/images/ser.png"/> View grade</h6>
+												<h6 className="Text-2-ex-lead" id="u331-17"><img className="defineImgs" src="/images/bed.png"/> Master bedroom size</h6>
+												<h6 className="Text-2-ex-lead" id="u331-19"><img className="defineImgs" src="/images/sofa.png"/> Living room size</h6>
+												<h6 className="Text-2-ex-lead" id="u331-20">&nbsp;</h6>
+												<p className="Text-1">… and desired amenities such as fireplace, laundry and outdoor space.</p>
+												<div className="amenityIconImgs">
+													<img className="amenityIcon" id="fireplace" src="/images/icon_fireplace_2_30k_33px.png"/>
+													<img className="amenityIcon" id="washer" src="/images/washer_3_37px.png"/>
+													<img className="amenityIcon" id="roofdeck" src="/images/icon_roofdeck_4_30k_33px.png"/>
+												</div>
+											</div>
+                    </div>
+                  </div>
+                </div>
+								<div className="col-md-5ths">
+                  <div className="panel">
+                    <div className="panel-body how-it-works-panel">
+                      <div className="clearfix grpelem" id="u333-22">
+												<h4 className="text-popups" id="u333-3"><span id="u333">Search</span><span id="u333-2"> for apartments</span></h4>
+												<h4 className="text-popups">that match it</h4>
+												<h4 className="text-popups">&nbsp;</h4>
+												<img id="pinkArrow" src="/images/search_homepik.png"/>
+												<p className="Text-1" id="searchText"><span id="u333-8">HomePik</span> will scan the entire database of available apartments in New York City for those that match your buying formula</p>
+											</div> 
+                    </div>
+                  </div>
+                </div>
+								<div className="col-md-5ths">
+                  <div className="panel">
+                    <div className="panel-body how-it-works-panel">
+                      <div className="clearfix grpelem" id="u334-20">
+												<h4 className="text-popups" id="u334-3"><span id="u334">View</span><span id="u334-2"> the results of</span></h4>
+												<h4 className="text-popups">the search</h4>
+												<p>&nbsp;</p>
+												<p className="Text-1"><span id="u334-7">HomePik</span> has located every apartment that meets your minimum criteria and compared them using its patented rating system:</p>
+												<p className="Text-1">&nbsp;</p>
+												<h6 className="Text-2-ex-lead" id="u334-12"><img className="viewImgs" src="/images/meets.png"/> = meets your criteria</h6>
+												<h6 className="Text-2-ex-lead" id="u334-14"><img className="viewImgs" src="/images/exceeds.png"/> = exceeds your criteria</h6>
+												<h6 className="Text-2-ex-lead"><img className="viewImgs" src="/images/greatly.png"/> = greatly exceeds your</h6>
+												<p className="Text-1" id="u334-18">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; criteria</p>
+												<img src="/images/divider_d.png"/>
+												<div className="Text-1 clearfix grpelem" id="u339-9">
+													<p id="u339-2">HomePik is the one</p>
+													<p id="u339-4">and only search engine able to compare properties.</p>
+													<p>&nbsp;</p>
+													<p>Your searches. Your way.</p>
+												</div>
+											</div>
+                    </div>
+                  </div>
+                </div>
+								<div className="col-md-5ths">
+                  <div className="panel">
+                    <div className="panel-body how-it-works-panel">
+                      <div className="clearfix grpelem" id="u335-18">
+												<h4 className="text-popups" id="u335-3"><span id="u335">Manage</span><span id="u335-2"> your searches</span></h4>
+												<h4 className="text-popups">and selections</h4>
+												<p>&nbsp;</p>
+												<p className="Text-1">Save searches and properties, email them, and connect with your agent</p>
+												<p className="Text-1">&nbsp;</p>
+												<h6 className="Text-2-ex-lead" id="u335-11"><img className="manageImgs" src="/images/save_to_folders.png"/> Save to folders</h6>
+												<h6 className="Text-2-ex-lead" id="u335-13"><img className="manageImgs" src="/images/emails.png"/> Email</h6>
+												<h6 className="Text-2-ex-lead" id="u335-15"><img className="manageImgs" src="/images/chose_your_agent.png"/> Choose an agent</h6>
+												<p>&nbsp;</p>
+											</div> 
+                    </div>
+                  </div>
+                </div>
 							</div>
-							<div className="container" id="videoDocumentSection">
-								<div className="row">
+							<div className="row">
+								<div>
+									<p className="about-phrase">HomePik is the future of real estate. If you're not using it, you're missing out.</p>
+									<p> </p>
+								</div>
+								<div className="col-md-3 video-image">
+									<p className="FAQ-1-grey-bd">Video Overview of HomePik</p>
 									<div>
-										<p className="about-phrase">HomePik is the future of real estate. If you're not using it, you're missing out.</p>
-										<p> </p>
+										<iframe id="video" className="video-container" src="https://www.youtube.com/embed/Yv_O-j-cRTo" frameBorder="0" allowFullScreen></iframe>
 									</div>
-									<div className="col-md-3 video-image">
-										<p className="FAQ-1-grey-bd" style={{marginBottom: 20 + "px"}}>Video Overview of HomePik</p>
-										<div>
-											<iframe id="video" className="video-container" src="https://www.youtube.com/embed/Yv_O-j-cRTo" frameBorder="0" allowFullScreen></iframe>
-										</div>
-									</div>
-									<div className="col-md-4 col-md-offset-2 pdf-image">
-										<p className="FAQ-1-grey-bd" style={{marginBottom: 20 + "px"}}>PowerPoint Presentation</p>
-										<div>
-											<a href="/documents/HomePik_Investor_Presentation.pdf"><img className="image-container" alt="press release" src="/images/investimage.JPG" title="PowerPoint Presentation" /></a>
-										</div>
+								</div>
+								<div className="col-md-4 col-md-offset-2 pdf-image">
+									<p className="FAQ-1-grey-bd">PowerPoint Presentation</p>
+									<div>
+										<a href="/documents/HomePik_Investor_Presentation.pdf"><img className="image-container" alt="press release" src="/images/investimage.JPG" title="PowerPoint Presentation" /></a>
 									</div>
 								</div>
 							</div>
