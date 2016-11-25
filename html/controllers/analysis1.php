5e6d02df126d08f26e4c4f1c80b88bd03f2948e2
webdev498 <gcmobile@mail.com>
2016-11-25 07:52:09 -0500

    change

--- html/controllers/analysis1.php
+++ html/controllers/analysis1.php
@@ -1852,7 +1852,7 @@ if (isset($_POST['agent-yearly'])) {
 	
 	//Total buyer listings
     //$sql = "SELECT COUNT(*) AS buyerCount FROM `users` WHERE P_agent = '" . $agentCode . "'";
-    $sql = "SELECT COUNT(*) AS listingCount, id FROM `users` WHERE rtime >= '" . $rtimeBegDate . "' AND rtime <= '" . $rtimeEndDate . "' AND (P_agent = '" . $agentYearlyCode . "' OR P_agent2 = '" . $agentYearlyCode . "')";
+    $sql = "SELECT COUNT(*) AS buyerListingCount, id FROM `users`, `saved_listings` WHERE email = user AND rtime >= '" . $rtimeBegDate . "' AND rtime <= '" . $rtimeEndDate . "' AND (P_agent = '" . $agentYearlyCode . "' OR P_agent2 = '" . $agentYearlyCode . "')";
     //$sql = "SELECT COUNT(*) AS buyerCount, id FROM `users` WHERE (P_agent = '" . $agentCode . "' OR P_agent2 = '" . $agentCode . "')";
     $result = mysql_query( $sql ) or die("Couldn't execute query. Total buyers.".mysql_error());
 
@@ -1868,10 +1868,10 @@ if (isset($_POST['agent-yearly'])) {
         $buyerAllListingsChart['rows'][] = array(
             'c' => array (
                  array('v' => 'Buyers'),
-                 array('v' => $row['buyerCount'])
+                 array('v' => $row['buyerListingCount'])
              )
         );
-        $buyerAllListingsChartTotal = $buyerAllListingsChartTotal + $row['buyerCount'];
+        $buyerAllListingsChartTotal = $buyerAllListingsChartTotal + $row['buyerListingCount'];
     }
 
 
@@ -2058,7 +2058,7 @@ if (isset($_POST['agent-yearly'])) {
           height: 400,
           legend: { position: "none" },
           hAxis: {
-            title: '',
+            title: 'Listings',
             minValue: 0,
           },
           vAxis: {
