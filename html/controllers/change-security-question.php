5b8d0b51a62d1e35aae32665b0c20ad9ae0fe04e
webdev498 <gcmobile@mail.com>
2017-01-23 09:00:19 -0500

    change security question page

--- html/controllers/change-security-question.php
+++ html/controllers/change-security-question.php
@@ -1,14 +1,17 @@
 <?php
 session_start();
+include_once("/controllers/dbconfig.php");
 include_once('/controllers/functions.php');
 include_once('/controllers/basicHead2.php');
+$con = mysql_connect($dbhost, $dbuser, $dbpassword) or die(mysql_error());
+$db = mysql_select_db('sp', $con) or die(mysql_error());
 
 if(isset($_SESSION['buyer'])){
 	$email = $_SESSION['email'];
 	$result = mysql_query( "SELECT security_question, security_answer FROM `users` WHERE email ='".$email."'" ) or die("Couldn't execute query.".mysql_error());
 	$row = mysql_fetch_array($result,MYSQL_ASSOC);
-	$securityQuestion = $row['security_question'];
-    $securityAnswer = $row['security_answer'];
+	$secQues = $row['security_question'];
+    $secAns = $row['security_answer'];
 }
 
 $mainPage = (isset($_GET['MP']) ? $_GET['MP'] : "");
@@ -29,8 +32,8 @@ $mainPage = (isset($_GET['MP']) ? $_GET['MP'] : "");
   var ChangeSecurity = React.createClass({
     getInitialState: function() {
       return{
-        secQues: <?php echo (isset($secQues) ? $secQues : "default") ?>,
-        secAns: <?php echo (isset($secAns) ? $secAns : "") ?>,
+        secQues: "<?php echo (isset($secQues) ? $secQues : "default") ?>",
+        secAns: "<?php echo (isset($secAns) ? $secAns : "") ?>",
         mainPage: "<?php echo (isset($mainPage) ? $mainPage : "") ?>"
       };
 	  },
@@ -79,24 +82,26 @@ $mainPage = (isset($_GET['MP']) ? $_GET['MP'] : "");
             <NavBar mainPage={this.state.mainPage}  />
             <AddressSearch mainPage={this.state.mainPage}  />
             <div className="Text-1" id="u8521-1">
-              <span className="Text-1" id="u8522-1">Change Security Question</span>
+			<span className="Text-1" id="u8522-1">Update Security Question</span> 
               <form onSubmit={this.validate} action="users/change-security.php" id="validate" className="validate" method="get">
                 <table cellPadding="2" cellSpacing="0" border="0">
-                  <colgroup><col width="250"/><col width="315"/></colgroup>
+                  <colgroup><col width="250"/><col width="300"/></colgroup>
                   <tbody>
                     <tr>
-						<td className="text-popups">Security Question:</td>
-						<td className="text-popups"><select id="formQuestion" className="input2" name="security-question" onChange={this.handleChange.bind(this, 'secQues')}>
-							<option value="default" selected="selected">Select A Security Question</option>
-							<option value="1">What is your middle name?</option>
-							<option value="2">What is your mother's maiden name?</option>
-							<option value="3">What was the name of the street where you grew up?</option>
-							<option value="4">What is your favorite food?</option>
-						</select></td>
+						<td className="text-popups security">Security Question:</td>
+                            <td>
+							<select id="secQues" name="security-question" value={this.state.secQues} onChange={this.handleChange.bind(this, 'secQues')}>
+								<option value="default">Select A Security Question</option>
+								<option value="1">What is your middle name?</option>
+								<option value="2">What is your mother's maiden name?</option>
+								<option value="3">What was the name of the street where you grew up?</option>
+								<option value="4">What is your favorite food?</option>
+                            </select>
+							</td>
 					</tr>
 					<tr id="answer">
-					  <td className="text-popups">Security Answer:</td>
-					  <td className="text-popups"><input type="text" id="formAnswer" className="grade_desc input1" name="security-answer" onChange={this.handleChange.bind(this, 'secAns')}/></td>
+					  <td className="text-popups security">Security Answer:</td>
+					  <td className="text-popups"><input type="text" value={this.state.secAns} id="formAnswer" className="grade_desc input1" name="security-answer" onChange={this.handleChange.bind(this, 'secAns')}/></td>
 					</tr>
                     <tr>
                       <td>&nbsp;</td>
